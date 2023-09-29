d2212b9183d360356f413859e130e6978df19bc4
morishitakosuke1028 <k.morishita1028@outlook.jp>
2023-09-30 06:35:52 +0900

    first commit

---
+++ server/school_support/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+					$table->id();
+					$table->integer('school_id');
+					$table->string('name');
+					$table->string('kana');
+					$table->string('email')->unique();
+					$table->timestamp('email_verified_at')->nullable();
+					$table->string('tel');
+					$table->integer('role')->comment('1=admin,2=editor,3=reader');
+					$table->date('retirement_date')->nullable();
+					$table->string('session_id')->unique();
+					$table->string('password');
+					$table->rememberToken();
+					$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ server/school_support/database/migrations/2023_09_29_212816_create_children_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+					$table->id();
+					$table->string('name');
+					$table->string('kana');
+					$table->string('school_id');
+					$table->string('gender')->nullable();
+					$table->string('zip')->nullable();
+					$table->string('address')->nullable();
+					$table->string('tel');
+					$table->date('birthday')->nullable();
+					$table->date('admission_date');
+					$table->date('movein_date')->nullable();
+					$table->date('graduation_date')->nullable();
+					$table->string('pin_code')->comment('保護者用閲覧制限');
+					$table->string('email')->unique();
+					$table->timestamp('email_verified_at')->nullable();
+					$table->string('session_id')->unique();
+					$table->string('password');
+					$table->rememberToken();
+					$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children');
+    }
+};
