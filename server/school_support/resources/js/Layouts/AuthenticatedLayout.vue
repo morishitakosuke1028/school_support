8cd477188fa90bec05337f2645294fc7218da016
morishitakosuke1028 <k.morishita1028@outlook.jp>
2023-10-17 23:10:34 +0900

    feat: admin登録実装途中

--- server/school_support/resources/js/Layouts/AuthenticatedLayout.vue
+++ server/school_support/resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,6 +33,11 @@ const showingNavigationDropdown = ref(false);
                                     Dashboard
                                 </NavLink>
                             </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <NavLink :href="route('register')" :active="route().current('register')">
+                                    職員登録
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
