cb399d067d7b6f48483698a66dac210cd76347a7
morishitakosuke1028 <k.morishita1028@outlook.jp>
2023-10-27 23:04:36 +0900

    feat: 生徒用パスワードリセット実装

--- server/school_support/app/Http/Controllers/Child/Auth/PasswordResetLinkController.php
+++ server/school_support/app/Http/Controllers/Child/Auth/PasswordResetLinkController.php
@@ -17,7 +17,7 @@ class PasswordResetLinkController extends Controller
      */
     public function create(): Response
     {
-        return Inertia::render('Auth/ForgotPassword', [
+        return Inertia::render('Child/Auth/ForgotPassword', [
             'status' => session('status'),
         ]);
     }
