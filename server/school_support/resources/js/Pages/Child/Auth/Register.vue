581651e6b9b05e012b755d01bdb9f27e20ef1d60
morishitakosuke1028 <k.morishita1028@outlook.jp>
2023-11-01 21:50:02 +0900

    feat: 生徒用登録画面にヘッダー実装

--- server/school_support/resources/js/Pages/Child/Auth/Register.vue
+++ server/school_support/resources/js/Pages/Child/Auth/Register.vue
@@ -1,4 +1,5 @@
 <script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
@@ -35,228 +36,230 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="生徒登録" />
+		<AuthenticatedLayout>
+				<GuestLayout>
+						<Head title="生徒登録" />
 
-				<h1 class="mt-2 mb-3">生徒用アカウント作成</h1>
+						<h1 class="mt-2 mb-3">生徒用アカウント作成</h1>
 
-        <form @submit.prevent="submit">
-					<TextInput
-								id="school_id"
-								type="hidden"
-								v-model="form.school_id"
-								required
-								autofocus
-								autocomplete="school_id"
-						/>
-            <div>
-                <InputLabel for="name" value="名前" />
-
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
-
-                <InputError class="mt-2" :message="form.errors.name" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="kana" value="かな" />
-
-                <TextInput
-                    id="kana"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.kana"
+						<form @submit.prevent="submit">
+							<TextInput
+										id="school_id"
+										type="hidden"
+										v-model="form.school_id"
 										required
-                    autofocus
-                    autocomplete="kana"
-                />
+										autofocus
+										autocomplete="school_id"
+								/>
+								<div>
+										<InputLabel for="name" value="名前" />
 
-                <InputError class="mt-2" :message="form.errors.kana" />
-            </div>
+										<TextInput
+												id="name"
+												type="text"
+												class="mt-1 block w-full"
+												v-model="form.name"
+												required
+												autofocus
+												autocomplete="name"
+										/>
 
-						<div class="mt-4">
-                <!-- <InputLabel for="gender" value="性別" /> -->
-                <InputLabel value="性別" />
-
-                <div class="mt-2">
-										<label for="gender_men" class="inline-flex items-center">
-												<input type="radio" id="gender_men" value="1" v-model="form.gender" required />
-												<span class="ml-2">男</span>
-										</label>
-										<label for="gender_women" class="ml-4 inline-flex items-center">
-												<input type="radio" id="gender_women" value="2" v-model="form.gender" required />
-												<span class="ml-2">女</span>
-										</label>
+										<InputError class="mt-2" :message="form.errors.name" />
 								</div>
 
-                <InputError class="mt-2" :message="form.errors.gender" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="kana" value="かな" />
 
-						<div class="mt-4">
-                <InputLabel for="zip" value="郵便番号" />
+										<TextInput
+												id="kana"
+												type="text"
+												class="mt-1 block w-full"
+												v-model="form.kana"
+												required
+												autofocus
+												autocomplete="kana"
+										/>
 
-                <TextInput
-                    id="zip"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.zip"
-                    autofocus
-                    autocomplete="zip"
-                />
+										<InputError class="mt-2" :message="form.errors.kana" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.zip" />
-            </div>
+								<div class="mt-4">
+										<!-- <InputLabel for="gender" value="性別" /> -->
+										<InputLabel value="性別" />
 
-						<div class="mt-4">
-                <InputLabel for="address" value="住所" />
+										<div class="mt-2">
+												<label for="gender_men" class="inline-flex items-center">
+														<input type="radio" id="gender_men" value="1" v-model="form.gender" required />
+														<span class="ml-2">男</span>
+												</label>
+												<label for="gender_women" class="ml-4 inline-flex items-center">
+														<input type="radio" id="gender_women" value="2" v-model="form.gender" required />
+														<span class="ml-2">女</span>
+												</label>
+										</div>
 
-                <TextInput
-                    id="address"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.address"
-                    autofocus
-                    autocomplete="address"
-                />
+										<InputError class="mt-2" :message="form.errors.gender" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.address" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="zip" value="郵便番号" />
 
-						<div class="mt-4">
-                <InputLabel for="birthday" value="生年月日" />
+										<TextInput
+												id="zip"
+												type="text"
+												class="mt-1 block w-full"
+												v-model="form.zip"
+												autofocus
+												autocomplete="zip"
+										/>
 
-                <TextInput
-                    id="birthday"
-                    type="date"
-                    class="mt-1 block w-full"
-                    v-model="form.birthday"
-                    autofocus
-                    autocomplete="birthday"
-                />
+										<InputError class="mt-2" :message="form.errors.zip" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.birthday" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="address" value="住所" />
 
-						<div class="mt-4">
-                <InputLabel for="admission_date" value="入学日" />
+										<TextInput
+												id="address"
+												type="text"
+												class="mt-1 block w-full"
+												v-model="form.address"
+												autofocus
+												autocomplete="address"
+										/>
 
-                <TextInput
-                    id="admission_date"
-                    type="date"
-                    class="mt-1 block w-full"
-                    v-model="form.admission_date"
-                    autofocus
-                    autocomplete="admission_date"
-                />
+										<InputError class="mt-2" :message="form.errors.address" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.admission_date" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="birthday" value="生年月日" />
 
-            <div class="mt-4">
-                <InputLabel for="email" value="保護者メールアドレス" />
+										<TextInput
+												id="birthday"
+												type="date"
+												class="mt-1 block w-full"
+												v-model="form.birthday"
+												autofocus
+												autocomplete="birthday"
+										/>
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
+										<InputError class="mt-2" :message="form.errors.birthday" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="admission_date" value="入学日" />
 
-            <div class="mt-4">
-                <InputLabel for="tel" value="電話番号" />
+										<TextInput
+												id="admission_date"
+												type="date"
+												class="mt-1 block w-full"
+												v-model="form.admission_date"
+												autofocus
+												autocomplete="admission_date"
+										/>
 
-                <TextInput
-                    id="tel"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.tel"
-										required
-                    autocomplete="tel"
-                />
+										<InputError class="mt-2" :message="form.errors.admission_date" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.tel" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="email" value="保護者メールアドレス" />
 
-            <div class="mt-4">
-                <InputLabel for="pin_code" value="簡易認証コード" />
+										<TextInput
+												id="email"
+												type="email"
+												class="mt-1 block w-full"
+												v-model="form.email"
+												required
+												autocomplete="username"
+										/>
 
-                <TextInput
-                    id="pin_code"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.pin_code"
-                    required
-                    autocomplete="pin_code"
-                />
+										<InputError class="mt-2" :message="form.errors.email" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.pin_code" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="tel" value="電話番号" />
 
-            <div class="mt-4">
-                <InputLabel for="session_id" value="認証 ID" />
+										<TextInput
+												id="tel"
+												type="text"
+												class="mt-1 block w-full"
+												v-model="form.tel"
+												required
+												autocomplete="tel"
+										/>
 
-                <TextInput
-                    id="session_id"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.session_id"
-                    required
-                    autocomplete="session_id"
-                />
+										<InputError class="mt-2" :message="form.errors.tel" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.session_id" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="pin_code" value="簡易認証コード" />
 
-            <div class="mt-4">
-                <InputLabel for="password" value="パスワード" />
+										<TextInput
+												id="pin_code"
+												type="text"
+												class="mt-1 block w-full"
+												v-model="form.pin_code"
+												required
+												autocomplete="pin_code"
+										/>
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
+										<InputError class="mt-2" :message="form.errors.pin_code" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="session_id" value="認証 ID" />
 
-            <div class="mt-4">
-                <InputLabel for="password_confirmation" value="パスワード確認" />
+										<TextInput
+												id="session_id"
+												type="text"
+												class="mt-1 block w-full"
+												v-model="form.session_id"
+												required
+												autocomplete="session_id"
+										/>
 
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
+										<InputError class="mt-2" :message="form.errors.session_id" />
+								</div>
 
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
+								<div class="mt-4">
+										<InputLabel for="password" value="パスワード" />
 
-            <div class="flex items-center justify-around mt-4">
-								<Link as="button" class="inline-flex items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-500 focus:bg-gray-500 active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" :href="route('dashboard')">
-										トップへ
-								</Link>
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    生徒登録
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+										<TextInput
+												id="password"
+												type="password"
+												class="mt-1 block w-full"
+												v-model="form.password"
+												required
+												autocomplete="new-password"
+										/>
+
+										<InputError class="mt-2" :message="form.errors.password" />
+								</div>
+
+								<div class="mt-4">
+										<InputLabel for="password_confirmation" value="パスワード確認" />
+
+										<TextInput
+												id="password_confirmation"
+												type="password"
+												class="mt-1 block w-full"
+												v-model="form.password_confirmation"
+												required
+												autocomplete="new-password"
+										/>
+
+										<InputError class="mt-2" :message="form.errors.password_confirmation" />
+								</div>
+
+								<div class="flex items-center justify-around mt-4">
+										<Link as="button" class="inline-flex items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-500 focus:bg-gray-500 active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" :href="route('dashboard')">
+												トップへ
+										</Link>
+										<PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+												生徒登録
+										</PrimaryButton>
+								</div>
+						</form>
+				</GuestLayout>
+		</AuthenticatedLayout>
 </template>
