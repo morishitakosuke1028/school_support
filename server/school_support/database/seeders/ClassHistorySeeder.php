e9e83b23d648362da31eb6f5ad75f99cc880913e
morishitakosuke1028 <k.morishita1028@outlook.jp>
2023-11-11 00:21:05 +0900

    feat: クラス替え関連ファイル生成

---
+++ server/school_support/database/seeders/ClassHistorySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClassHistorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
