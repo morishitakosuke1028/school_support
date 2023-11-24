68cf9229d2a23aa89ba529cc417dbea263c70dca
morishitakosuke1028 <k.morishita1028@outlook.jp>
2023-11-24 22:33:13 +0900

    feat: 生徒ダミーデータ挿入

--- server/school_support/resources/js/Pages/GradeClassHistory/Edit.vue
+++ server/school_support/resources/js/Pages/GradeClassHistory/Edit.vue
@@ -9,8 +9,6 @@ const props = defineProps({
     users: Object,
 })
 
-// const selectedChildren = ref([]);
-
 const form = reactive({
     id: props.gradeClassHistory.id,
     user_id: props.gradeClassHistory.user_id,
@@ -41,7 +39,8 @@ const deleteGradeClassHistory = id => {
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <section class="text-gray-600 body-font relative">
+                        <section class="text-gray-600 body-font relative flex-auto">
+
                             <form @submit.prevent="updateGradeClassHistory(form.id)">
                                 <div class="container px-5 py-8 mx-auto">
                                     <div class="lg:w-1/2 md:w-2/3 mx-auto">
@@ -57,15 +56,27 @@ const deleteGradeClassHistory = id => {
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="p-2 w-full">
-                                                <div class="relative">
-                                                    <label for="child_id" class="leading-7 text-sm text-gray-600">生徒名</label>
-                                                    <span class="font-medium text-sm text-red-700">　(必須)</span>
-                                                    <div>
-                                                        <!-- <select multiple v-model="selectedChildren.value"> -->
-                                                        <select multiple v-model="form.child_id">
-                                                            <option v-for="child in props.children" :key="child.id" :value="child.id">{{ child.name }}</option>
-                                                        </select>
+                                            <div class="flex flex-wrap -m-2">
+                                                <div class="p-2 w-full">
+                                                    <div class="relative">
+                                                        <label for="child_id" class="leading-7 text-sm text-gray-600">生徒名</label>
+                                                        <span class="font-medium text-sm text-red-700">　(必須)</span>
+                                                        <div>
+                                                            <select multiple>
+                                                                <option v-for="child in props.children" :key="child.id" :value="child.id">{{ child.name }}</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="p-2 w-full">
+                                                    <div class="relative">
+                                                        <label for="child_id" class="leading-7 text-sm text-gray-600">生徒名</label>
+                                                        <span class="font-medium text-sm text-red-700">　(必須)</span>
+                                                        <div>
+                                                            <select multiple v-model="form.child_id">
+                                                                <option v-for="child in props.children" :key="child.id" :value="child.id">{{ child.name }}</option>
+                                                            </select>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
