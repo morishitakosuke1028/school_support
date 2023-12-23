3188ad7304b0a91b59ac2146c9c1d0c1d6b85e35
morishitakosuke1028 <k.morishita1028@outlook.jp>
2023-12-24 00:44:02 +0900

    feat: 生徒側 宿題マスタコントローラ生成

---
+++ server/school_support/app/Http/Controllers/Child/HomeworkController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Child;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class HomeworkController extends Controller
+{
+    //
+}
