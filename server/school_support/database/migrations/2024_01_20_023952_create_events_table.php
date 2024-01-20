e2f6e254b59270ff9c62002e961f1948e158690a
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-01-20 10:12:01 +0900

    feat: DBテーブル作成

--- server/school_support/database/migrations/2024_01_20_023952_create_events_table.php
+++ server/school_support/database/migrations/2024_01_20_023952_create_events_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('grade_class_id')->constrained();
+            $table->datetime('start_datetime');
+            $table->datetime('end_datetime');
+            $table->string('title');
+            $table->string('place');
+            $table->string('personal_effect');
+            $table->text('content');
             $table->timestamps();
         });
     }
