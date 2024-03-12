abd6a71cde38458af2e6ded87f6816b6d7ce9c7a
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-03-12 11:28:56 +0900

    feat: トップページ実装途中

--- server/school_support/app/Http/Controllers/StaticController.php
+++ server/school_support/app/Http/Controllers/StaticController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class StaticController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return Inertia::render('Index');
+    }
 }
---
+++ server/school_support/resources/js/Pages/Index.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+</script>
+<template>
+    <Head title="すくサポ　公式" />
+
+    <div class="container mx-auto px-4">
+        <main>
+            <section id="features" class="my-10">
+                <h2 class="text-2xl font-bold text-center">主な機能</h2>
+                <!-- 機能一覧の内容 -->
+            </section>
+            <section id="testimonials" class="my-10">
+                <h2 class="text-2xl font-bold text-center">ユーザー様の声</h2>
+                <div class="flex justify-center items-center h-screen">
+                    <div class="bg-center bg-cover m-4">
+                        <img src="images/admin_login.webp" alt="">
+                    </div>
+                    <Link as="button" class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg ml-5" :href="route('login')">
+                        管理側ログイン
+                    </Link>
+                    <div class="bg-center bg-cover m-4">
+                        <img src="images/child_login.webp" alt="">
+                    </div>
+                    <Link as="button" class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg ml-5" :href="route('child.login')">
+                        生徒（保護者）ログイン
+                    </Link>
+                </div>
+            </section>
+            <section id="faq" class="my-10">
+            <h2 class="text-2xl font-bold text-center">よくあるご質問</h2>
+            <!-- FAQの内容 -->
+            </section>
+        </main>
+        <footer class="text-center py-5 bg-gray-200">
+            © 2024 すくサポ. All rights reserved.
+        </footer>
+    </div>
+  </template>
