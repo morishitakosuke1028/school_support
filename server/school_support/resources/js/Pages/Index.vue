89f0aab5accd5afa6535a5ec82961a2074ebae0a
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-03-14 12:16:01 +0900

    feat: トップページ実装途中

--- server/school_support/resources/js/Pages/Index.vue
+++ server/school_support/resources/js/Pages/Index.vue
@@ -14,24 +14,37 @@ onMounted(() => {
     setTimeout(() => showItem.value.text1 = true, 600);
     setTimeout(() => showItem.value.text2 = true, 1100);
     setTimeout(() => showItem.value.image = true, 1600);
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('slide-up');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {threshold: 0.4});
+
+    document.querySelectorAll('.animatable').forEach(el => {
+        observer.observe(el);
+    });
 });
 </script>
 <template>
     <Head title="すくサポ　公式" />
 
-    <div class="flex flex-col md:flex-row w-full min-h-screen">
+    <div class="flex flex-col md:flex-row w-full md:min-h-screen-md\:null">
         <div class="md:w-1/2 w-full flex justify-center items-center">
             <img src="images/kv_pc.jpg" class="md:h-full h-auto md:w-full object-cover fade-in" alt="">
         </div>
         <div class="flex w-full md:w-1/2 justify-center">
             <div>
-                <h1 :class="showItem.logo ? 'slide-in' : 'hidden-start'" class="mx-auto md:ml-24 mb-12">
-                    <img src="images/logo.png" alt="">
+                <h1 :class="showItem.logo ? 'slide-in' : 'hidden-start'" class="mx-auto md:ml-24 mb-12 logo-container">
+                    <img src="images/logo.png" class="logo-image" alt="">
                 </h1>
-                <p :class="showItem.text1 ? 'slide-in' : 'hidden-start'" class="text-center bg-gray-200" style="font-size: 2em; color: cornflowerblue;">
+                <p :class="showItem.text1 ? 'slide-in' : 'hidden-start'" class="text-center bg-gray-200 px-1 text-top">
                     小学校の<span class="font-bold" style="font-size: 1.5em; color: #00bfff;">当たり前</span>を
                 </p>
-                <p :class="showItem.text2 ? 'slide-in' : 'hidden-start'" class="text-center bg-gray-200 px-3" style="font-size: 2em; color: cornflowerblue;">
+                <p :class="showItem.text2 ? 'slide-in' : 'hidden-start'" class="text-center bg-gray-200 px-3 text-bottom">
                     <span class="font-bold" style="font-size: 1.5em; color: #00bfff;">変える</span>サポートシステム
                 </p>
                 <div :class="showItem.image ? 'slide-in' : 'hidden-start'" class="text-center mt-10">
@@ -42,29 +55,31 @@ onMounted(() => {
     </div>
     <div class="container mx-auto">
         <main class="w-full my-10">
-            <section id="features" class="my-10">
-                <h2 class="text-2xl font-bold text-center">主な機能</h2>
+            <section id="features" class="animatable my-10">
+                <h2 class="main-background text-2xl font-bold text-center">主な機能</h2>
                 <!-- 機能一覧の内容 -->
             </section>
-            <section id="testimonials" class="my-10">
-                <h2 class="text-2xl font-bold text-center">ログイン</h2>
-                <div class="flex justify-center items-center h-screen">
-                    <div class="bg-center bg-cover m-4">
-                        <img src="images/admin_login.webp" alt="">
+            <section id="testimonials" class="animatable my-10">
+                <h2 class="login-background text-2xl font-bold text-center">ログイン</h2>
+                <div class="md:flex justify-center items-center">
+                    <div class="bg-center bg-cover m-4 relative">
+                        <h3 class="overlay-text">学校職員の方はこちら</h3>
+                        <img src="images/admin-login.jpg" class="admin-login-image" alt="">
+                        <Link as="button" class="link-button text-white bg-blue-300 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg" :href="route('login')">
+                            ログイン
+                        </Link>
                     </div>
-                    <Link as="button" class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg ml-5" :href="route('login')">
-                        管理側ログイン
-                    </Link>
-                    <div class="bg-center bg-cover m-4">
-                        <img src="images/child_login.webp" alt="">
+                    <div class="bg-center bg-cover m-4 relative">
+                        <h3 class="overlay-text">保護者の方はこちら</h3>
+                        <img src="images/child-login.jpg" class="child-login-image" alt="">
+                        <Link as="button" class="link-button text-white bg-blue-300 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg" :href="route('child.login')">
+                            ログイン
+                        </Link>
                     </div>
-                    <Link as="button" class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg ml-5" :href="route('child.login')">
-                        生徒（保護者）ログイン
-                    </Link>
                 </div>
             </section>
-            <section id="faq" class="my-10">
-            <h2 class="text-2xl font-bold text-center">よくあるご質問</h2>
+            <section id="faq" class="animatable my-10">
+            <h2 class="question-background text-2xl font-bold text-center">よくあるご質問</h2>
             <!-- FAQの内容 -->
             </section>
         </main>
@@ -77,7 +92,6 @@ onMounted(() => {
     .fade-in {
         animation: fadeInAnimation 2s ease-in forwards;
     }
-
     @keyframes fadeInAnimation {
         from {
         opacity: 0;
@@ -86,18 +100,23 @@ onMounted(() => {
         opacity: 1;
         }
     }
-
     .hidden-start {
         opacity: 0;
         transform: translateX(-50px);
     }
-
     .slide-in {
         opacity: 0;
         transform: translateX(-50px);
         animation: slideInAnimation 0.5s forwards;
     }
-
+    .text-top {
+        font-size: 2em;
+        color: cornflowerblue;
+    }
+    .text-bottom {
+        font-size: 2em;
+        color: cornflowerblue;
+    }
     @keyframes slideInAnimation {
         from {
             opacity: 0;
@@ -106,6 +125,168 @@ onMounted(() => {
         to {
             opacity: 1;
             transform: translateX(0);
+        }
+    }
+    .animatable {
+        opacity: 0;
+        transform: translateY(20px);
+        visibility: hidden;
+    }
+    .slide-up {
+        opacity: 1;
+        transform: translateY(0);
+        visibility: visible;
+        animation: slideUpAnimation 1s forwards;
+    }
+    @keyframes slideUpAnimation {
+        0% {
+            opacity: 0;
+            transform: translateY(20px);
+            visibility: visible;
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+    .main-background {
+        position: relative;
+        overflow: hidden;
+        padding: 1.5em 0;
+    }
+    .main-background::before {
+        content: 'MAIN';
+        position: absolute;
+        top: 35%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 2em;
+        color: rgba(240, 248, 255, 1);
+        z-index: -1;
+    }
+    .login-background {
+        position: relative;
+        overflow: hidden;
+        padding: 1.5em 0;
+    }
+    .login-background::before {
+        content: 'LOGIN';
+        position: absolute;
+        top: 35%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 2em;
+        color: rgba(240, 248, 255, 1);
+        z-index: -1;
+    }
+    .admin-login-image {
+        width: 600px;
+        height: 400px;
+    }
+    .child-login-image {
+        width: 600px;
+        height: 400px;
+    }
+    .overlay-text {
+        position: absolute;
+        top: 40px;
+        left: 50%;
+        transform: translateX(-50%);
+        padding-bottom: 10px;
+        color: cornflowerblue;
+        border-bottom: 3px dashed #4169e1;
+        font-weight: bold;
+        font-size: 1.5em;
+    }
+    .link-button {
+        position: absolute;
+        bottom: 20px;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    .relative {
+        position: relative;
+    }
+    .question-background {
+        position: relative;
+        overflow: hidden;
+        padding: 1.5em 0;
+    }
+    .question-background::before {
+        content: 'QUESTION';
+        position: absolute;
+        top: 35%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 2em;
+        color: rgba(240, 248, 255, 1);
+        z-index: -1;
+    }
+
+    @media only screen and (max-width: 960px) {
+        .fade-in {
+            animation: fadeInAnimation 2s ease-in forwards;
+            height: 600px;
+        }
+        .logo-image {
+            width: 50%;
+        }
+        .text-top {
+            font-size: 1.8em;
+            color: cornflowerblue;
+        }
+        .text-bottom {
+            font-size: 1.8em;
+            color: cornflowerblue;
+        }
+        .overlay-text {
+            position: absolute;
+            top: 40px;
+            left: 50%;
+            transform: translateX(-50%);
+            padding-bottom: 10px;
+            color: cornflowerblue;
+            border-bottom: 3px dashed #4169e1;
+            font-weight: bold;
+            font-size: 1em;
+        }
+    }
+    @media only screen and (max-width: 520px) {
+        .fade-in {
+            animation: fadeInAnimation 2s ease-in forwards;
+            height: 500px;
+        }
+        .logo-container {
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: 99;
+            width: 100%;
+        }
+        .logo-image {
+            position: relative;
+            width: 25%;
+            margin: 10px;
+            border-radius: 25%;
+        }
+        .text-top {
+            font-size: 1.5em;
+            color: cornflowerblue;
+        }
+        .text-bottom {
+            font-size: 1.5em;
+            color: cornflowerblue;
+        }
+        .overlay-text {
+            position: absolute;
+            top: 40px;
+            left: 50%;
+            transform: translateX(-50%);
+            padding-bottom: 10px;
+            color: cornflowerblue;
+            border-bottom: 3px dashed #4169e1;
+            font-weight: bold;
+            font-size: 1em;
         }
     }
 </style>
