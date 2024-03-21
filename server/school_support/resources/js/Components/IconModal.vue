6f0823a58ef311f8d1114c864f17185b813e66e2
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-03-21 11:44:14 +0900

    feat: トップページ実装

--- server/school_support/resources/js/Components/IconModal.vue
+++ server/school_support/resources/js/Components/IconModal.vue
@@ -10,7 +10,7 @@ defineProps({
         <span class="close" @click="$emit('close')">&times;</span>
         <div class="mx-auto my-auto">
             <h3 class="font-bold">登校一覧機能（出席簿）</h3>
-            <p class="text-gray-300 text-md">こちらは出席簿を一括で登録する機能となります。保護者様からの申請を自動で反映致します。</p>
+            <p class="text-gray-300 text-md my-2">こちらは出席簿を一括で登録する機能となります。保護者様からの申請を自動で反映致します。</p>
             <div class="md:flex">
                 <p><img src="images/attendance-child.png" alt=""><span class="text-xs text-gray-300">保護者サイト</span></p>
                 <p><img src="images/attendance-register.png" alt=""></p>
@@ -21,9 +21,9 @@ defineProps({
         <span class="close" @click="$emit('close')">&times;</span>
         <div class="mx-auto my-auto">
             <h3 class="font-bold">生徒関連の機能</h3>
-            <p class="text-gray-300 text-md">生徒の登録、編集などを行なえます。また登録した情報は保護者様のログイン時に使用できます。</p>
+            <p class="text-gray-300 text-md my-2">生徒の登録、編集などを行なえます。また登録した情報は保護者様のログイン時に使用できます。</p>
             <div class="md:flex">
-                <p><img src="images/child-register.png" alt=""><span class="text-xs text-gray-300">保護者サイト</span></p>
+                <p><img src="images/child-register.png" alt=""></p>
                 <p><img src="images/child-index.png" alt=""></p>
             </div>
         </div>
@@ -32,10 +32,20 @@ defineProps({
         <span class="close" @click="$emit('close')">&times;</span>
         <div class="mx-auto my-auto">
             <h3 class="font-bold">職員関連の機能</h3>
-            <p class="text-gray-300 text-md">職員の登録、編集などを行なえます。また登録した情報は職員様のログイン時に使用できます。</p>
+            <p class="text-gray-300 text-md my-2">職員の登録、編集などを行なえます。また登録した情報は職員様のログイン時に使用できます。</p>
             <div class="md:flex">
                 <p><img src="images/staff-register.png" alt=""></p>
                 <p><img src="images/staff-index.png" alt=""></p>
+            </div>
+        </div>
+      </div>
+      <div v-if="iconName === 'csvimport'" class="modal-content">
+        <span class="close" @click="$emit('close')">&times;</span>
+        <div class="mx-auto my-auto">
+            <h3 class="font-bold">生徒のCSVインポート</h3>
+            <p class="text-gray-300 text-md my-2">生徒の一括登録を行なえます。また登録した情報は生徒一覧で確認できます。</p>
+            <div class="md:flex">
+                <p><img src="images/csv-import-create.png" alt=""></p>
             </div>
         </div>
       </div>
