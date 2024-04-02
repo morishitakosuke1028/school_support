a088425b8fc7b8489bb1dbec38f2fc034fe78e19
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-04-02 12:05:31 +0900

    feat: テーブル作成

--- server/school_support/database/migrations/2024_03_30_102442_create_schedules_table.php
+++ server/school_support/database/migrations/2024_03_30_102442_create_schedules_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('schedules', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('grade_class_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('subject_id_first')->nullable()->constrained('subjects')->onDelete('set null');
+            $table->unsignedBigInteger('subject_id_second')->nullable()->constrained('subjects')->onDelete('set null');
+            $table->unsignedBigInteger('subject_id_third')->nullable()->constrained('subjects')->onDelete('set null');
+            $table->unsignedBigInteger('subject_id_fourth')->nullable()->constrained('subjects')->onDelete('set null');
+            $table->unsignedBigInteger('subject_id_five')->nullable()->constrained('subjects')->onDelete('set null');
+            $table->unsignedBigInteger('subject_id_six')->nullable()->constrained('subjects')->onDelete('set null');
+            $table->unsignedBigInteger('subject_id_other')->nullable()->constrained('subjects')->onDelete('set null');
+            $table->unsignedBigInteger('subject_id_all_check')->nullable()->constrained('subjects')->onDelete('set null');
+            $table->date('schedule_date');
             $table->timestamps();
         });
     }
