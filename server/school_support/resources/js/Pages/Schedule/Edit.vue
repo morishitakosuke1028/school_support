a2f441e5be233f4c17b09b9cd7bdc96ac43a5cbf
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-04-16 11:49:53 +0900

    feat: 時間割実装途中

---
+++ server/school_support/resources/js/Pages/Schedule/Edit.vue
@@ -0,0 +1,43 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link, router, usePage } from '@inertiajs/vue3';
+import { reactive, computed, watch, onMounted } from 'vue';
+
+
+const submitForm = async () => {
+
+};
+</script>
+
+<template>
+    <Head title="時間割編集" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                時間割編集
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <section class="text-gray-600 body-font relative">
+
+                            <form @submit.prevent="submitForm">
+
+                                <div class="my-5 text-center">
+                                    <button class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg mr-5">登録</button>
+                                    <Link as="button" class="text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg ml-5" :href="route('schedules.index')">
+                                        戻る
+                                    </Link>
+                                </div>
+                            </form>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
