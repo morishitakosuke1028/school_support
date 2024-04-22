b082958ab216ae41be5aaedbcb866ae5f3213622
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-04-22 22:05:09 +0900

    feat: 時間割実装途中

--- server/school_support/resources/js/Pages/Schedule/Edit.vue
+++ server/school_support/resources/js/Pages/Schedule/Edit.vue
@@ -8,7 +8,20 @@ const props = defineProps({
     subjects: Object,
 });
 
-const drag = ref(false);
+const data = ref([
+  {
+    id: 1,
+    content: "test1",
+  },
+  {
+    id: 2,
+    content: "test2",
+  },
+  {
+    id: 3,
+    content: "teat3",
+  },
+]);
 
 onMounted(() => {
   console.log(props.subjects);  // props.subjects の内容を確認
@@ -19,12 +32,16 @@ const submitForm = async () => {
 };
 </script>
 <style scoped>
-.draggable-item {
-  cursor: pointer;
+.drag-area {
+  list-style-type: none;
+  padding: 0;
+}
+.drag-item {
   padding: 10px;
+  margin: 5px 0;
+  background-color: #f4f4f4;
   border: 1px solid #ccc;
-  margin-bottom: 5px;
-  background-color: white;
+  cursor: grab;
 }
 </style>
 <template>
@@ -44,12 +61,19 @@ const submitForm = async () => {
                         <section class="text-gray-600 body-font relative">
 
                             <form @submit.prevent="submitForm">
-                                <draggable v-model="props.subjects" group="subjects" @start="drag=true" @end="drag=false">
+                                <!-- <draggable v-model="props.subjects" item-key="id" class="drag-area" tag="ul"> -->
                                     <!-- <transition-group>
                                         <div v-for="subject in props.subjects" :key="subject.id" class="draggable-item">
                                             <input type="text" v-model="subject.name" readonly>
                                         </div>
                                     </transition-group> -->
+                                <!-- </draggable> -->
+                                <draggable v-model="data" item-key="id">
+                                <template #item="{ element }">
+                                    <div class="drag-item">
+                                    {{ element.id }}
+                                    </div>
+                                </template>
                                 </draggable>
 
                                 <div class="my-5 text-center">
