d223bec58a6d4cf1227eaeef8cc70a6476cf3e76
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-05-04 21:52:40 +0900

    feat: 時間割機能実装途中

--- server/school_support/app/Http/Controllers/ScheduleController.php
+++ server/school_support/app/Http/Controllers/ScheduleController.php
@@ -27,9 +27,58 @@ class ScheduleController extends Controller
         ]);
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Schedule  $schedule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($gradeClassId)
+    {
+        $gradeClass = GradeClass::findOrFail($gradeClassId);
+        $subjects = subject::select('id', 'name')->get();
+        $schedules = Schedule::where('grade_class_id', $gradeClassId)->get();
+        return Inertia::render('Schedule/Edit', [
+            'gradeClass' => $gradeClass,
+            'subjects' => $subjects,
+            'schedules' => $schedules
+        ]);
+    }
+
     public function bulkStore(Request $request)
     {
+        \Log::info($request->all());
 
+        $scheduleEntries = $request->input('scheduleData');
 
+        foreach ($scheduleEntries as $date => $data) {
+            $data['schedule_day'] = $date;
+            $validator = Validator::make($data, [
+                'grade_class_id' => 'required|integer',
+                'schedule_day' => 'required',
+                'subject_id_first' => 'nullable|string',
+                'subject_id_second' => 'nullable|string',
+                'subject_id_third' => 'nullable|string',
+                'subject_id_fourth' => 'nullable|string',
+                'subject_id_five' => 'nullable|string',
+                'subject_id_six' => 'nullable|string',
+                'subject_id_other' => 'nullable|string',
+                'subject_id_all_check' => 'nullable|string',
+            ]);
+
+            if ($validator->fails()) {
+                return back()
+                    ->withErrors($validator)
+                    ->withInput();
+            }
+
+            // ここでデータを保存
+            Schedule::updateOrCreate(['schedule_day' => $date, 'grade_class_id' => $data['grade_class_id']], $data);
+        }
+
+        return to_route('schedules.index')->with([
+            'message' => '一括登録が完了しました。',
+            'status' => 'success',
+        ]);
     }
 }
