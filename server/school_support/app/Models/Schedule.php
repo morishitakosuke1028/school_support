8e9220a4ab2dac140c89f632a4d2e269ff315301
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-06-12 23:28:42 +0900

    feat: 時間割登録実装途中

--- server/school_support/app/Models/Schedule.php
+++ server/school_support/app/Models/Schedule.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'grade_class_id',
+        'subject_id_first',
+        'subject_id_second',
+        'subject_id_third',
+        'subject_id_fourth',
+        'subject_id_five',
+        'subject_id_six',
+        'subject_id_other',
+        'subject_id_all_check',
+        'schedule_date',
+    ];
 }
