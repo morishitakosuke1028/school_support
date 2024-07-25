95202491d3cec3b539f123c8f3c1a190ff56ff19
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-07-26 07:40:59 +0900

    feat: 学年クラスのindexメソッドテスト

---
+++ server/school_support/tests/Feature/GradeClassTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\GradeClass;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class GradeClassTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * テストユーザーの作成
+     *
+     * @return \App\Models\User
+     */
+    protected function createUser()
+    {
+        return User::factory()->create([
+            'role' => 1,
+        ]);
+    }
+
+    /**
+     * Index メソッドのテスト
+     *
+     * @return void
+     */
+    public function test_index()
+    {
+        $user = $this->createUser();
+        Auth::login($user);
+
+        GradeClass::factory()->count(3)->create();
+
+        $response = $this->actingAs($user)->get(route('gradeClasses.index'));
+
+        $response->assertStatus(200);
+        $response->assertInertia(function ($page) {
+            $page->component('GradeClass/Index')
+                ->has('gradeClasses.data', 3)
+                ->where('currentUserRole', true);
+        });
+    }
+}
