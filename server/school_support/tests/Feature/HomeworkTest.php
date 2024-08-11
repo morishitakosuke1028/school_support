ba82467f0f4ee647cbfbf61aab79cd94e086161e
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-08-12 08:58:36 +0900

    feat: 宿題一覧テスト

--- server/school_support/tests/Feature/HomeworkTest.php
+++ server/school_support/tests/Feature/HomeworkTest.php
@@ -5,16 +5,44 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\GradeClass;
+use App\Models\Homework;
 
 class HomeworkTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic feature test example.
+     * テストユーザーの作成
+     *
+     * @return \App\Models\User
      */
-    public function test_example(): void
+    protected function createUser()
     {
-        $response = $this->get('/');
+        return User::factory()->create([
+            'role' => 1,
+        ]);
+    }
+
+    /**
+     * Index メソッドのテスト
+     *
+     * @return void
+     */
+    public function test_index()
+    {
+        $user = $this->createUser();
+        $this->actingAs($user);
+
+        GradeClass::factory()->count(3)->create();
+
+        $response = $this->actingAs($user)->get(route('homeworks.index'));
 
         $response->assertStatus(200);
+        $response->assertInertia(function ($page) {
+            $page->component('Homework/Index')
+                ->has('gradeClasses.data', 3)
+                ->where('currentUserRole', true);
+        });
     }
 }
