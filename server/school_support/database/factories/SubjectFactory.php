1c4ca1537c8ec3ff993700a5fda2df6543c9351a
morishitakosuke1028 <k.morishita1028@outlook.jp>
2024-09-11 15:21:29 +0900

    feat: 教科マスタの一覧テスト

---
+++ server/school_support/database/factories/SubjectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Subject;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Subject>
+ */
+class SubjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->randomElement([
+                '算数', '国語', '理科'
+            ]),
+        ];
+    }
+}
--- server/school_support/tests/Feature/SubjectTest.php
+++ server/school_support/tests/Feature/SubjectTest.php
@@ -5,16 +5,46 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Subject;
 
 class SubjectTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic feature test example.
+     * テストユーザーの作成
+     *
+     * @return \App\Models\User
      */
-    public function test_example(): void
+    protected function createUser()
     {
-        $response = $this->get('/');
+        return User::factory()->create([
+            'role' => 1,
+        ]);
+    }
+
+    /**
+     * Index メソッドのテスト
+     *
+     * @return void
+     */
+    public function test_index()
+    {
+        $user = $this->createUser();
+        $this->actingAs($user);
+
+        Subject::query()->delete();
+
+        // テストデータを作成
+        Subject::factory()->count(3)->create();
+
+        $response = $this->get(route('subjects.index'));
 
         $response->assertStatus(200);
+        $response->assertInertia(function ($page) {
+            $page->component('Subject/Index')
+                ->where('currentUserRole', true);
+        });
     }
 }
